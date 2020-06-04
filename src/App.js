f270859d39422794bbdd1b26f10234823a1d07b5
cooljasonmelton <cooljasonmelton>
2020-06-03 22:44:12 -0500

    styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Jason Melton <em>Jason Melton</em> <b>Jason Melton</b></h1>
+      <p>Jason Melton <em>Jason Melton</em> <b>Jason Melton</b></p>
+      <Button> Jason Melton </Button>
+
+
     </div>
   );
 }
