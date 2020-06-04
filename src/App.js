fbed15e3790acfe7d409743b6819128544b040ff
cooljasonmelton <cooljasonmelton>
2020-06-04 01:17:28 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Button } from 'semantic-ui-react'
+import Navbar from './Navbar'
+ 
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+
+      
       <h1>Jason Melton <em>Jason Melton</em> <b>Jason Melton</b></h1>
       <p>Jason Melton <em>Jason Melton</em> <b>Jason Melton</b></p>
       <Button> Jason Melton </Button>
---
+++ src/Navbar.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Segment } from 'semantic-ui-react'
+import './Navbar.css';
+import blackstar from './pictures/blackstar.png';
+ 
+
+class Navbar extends React.Component{
+  state = {}
+
+  handleClick = e => {
+    this.setState({
+      activeItem: e.target.dataset.space
+    })
+  } 
+
+  render(){
+    const {activeItem} = this.state
+    return (
+      <Segment className="navbar-container">
+
+        <Segment className="navbar-item" >
+          <h3 className={activeItem === "bio" ? "clicked" : ""} 
+           onClick={e => this.handleClick(e)}
+           data-space="bio" 
+          >
+            Bio
+          </h3>
+        </Segment>
+
+        <img src={blackstar} className='navbar-star' alt='star'/>
+
+        <Segment className="navbar-item" >
+          <h3 className={activeItem === "shows" ? "clicked" : ""} 
+           onClick={e => this.handleClick(e)}
+           data-space="shows" 
+          >
+            Upcoming Shows
+          </h3>
+        </Segment>
+
+        <img src={blackstar} className='navbar-star' alt='star'/>
+
+        <Segment className="navbar-item" >
+          <h3 className={activeItem === "cmj" ? "clicked" : ""} 
+           onClick={this.handleClick}
+           data-space="cmj" 
+          >
+            Cocaine Murder Jam (sketches)
+          </h3>
+        </Segment>
+
+        <img src={blackstar} className='navbar-star' alt='star'/>
+
+        <Segment className="navbar-item" >
+          <h3 className={activeItem === "write" ? "clicked" : ""} 
+           onClick={e => this.handleClick(e)}
+           data-space="write" 
+          >
+            Misc
+          </h3>
+        </Segment>
+      </Segment>
+    );
+
+  }
+}
+
+export default Navbar;
